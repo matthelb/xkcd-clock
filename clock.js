bba0660b59bfeefb2885435f6ec71582af675e33
Matthew Burke <matthelb@usc.edu>
2014-04-09 00:56:02 -0700

    Update loading of clock image

--- clock.js
+++ clock.js
@@ -5,26 +5,33 @@ var SECONDS_IN_DAY = 60 * 60 * 24;
 function getRotation(seconds) {
 	return 2 * Math.PI * seconds / SECONDS_IN_DAY;
 }
+
 document.addEventListener('DOMContentLoaded', function(){
 	var clockImage = document.getElementById('clock-image');
-	var clockCanvas = document.getElementById('clock-canvas');
-	clockCanvas.width = clockImage.width;
-	clockCanvas.height = clockImage.height;
-	setInterval(function() {
-		var clockCanvasContext = clockCanvas.getContext('2d');
-		clockCanvasContext.clearRect(0, 0, clockCanvas.width, clockCanvas.height);
-		clockCanvasContext.drawImage(clockImage, 0, 0);
-		clockCanvasContext.save();
-		var date = new Date();
-		var seconds = date.getUTCSeconds();
-		seconds += date.getUTCMinutes() * 60;
-		seconds += date.getUTCHours() * 3600;
-		clockCanvasContext.beginPath();
-		clockCanvasContext.arc(clockCanvas.width / 2, clockCanvas.height / 2, MASK_RADIUS, 0, 2 * Math.PI, false);
-		clockCanvasContext.clip();
-		clockCanvasContext.translate(clockCanvas.width / 2, clockCanvas.height / 2);
-		clockCanvasContext.rotate(getRotation(seconds));
-		clockCanvasContext.drawImage(clockImage, -clockCanvas.width / 2, -clockCanvas.height / 2);
-		clockCanvasContext.restore();
-	}, INTERVAL * 1000);
+	clockImage.onload = function(){
+		var clockCanvas = document.getElementById('clock-canvas');
+		clockCanvas.width = clockImage.width;
+		clockCanvas.height = clockImage.height;
+		setInterval(function() {
+			var clockCanvasContext = clockCanvas.getContext('2d');
+			clockCanvasContext.clearRect(0, 0, clockCanvas.width, clockCanvas.height);
+			clockCanvasContext.drawImage(clockImage, 0, 0);
+			clockCanvasContext.save();
+			var date = new Date();
+			var seconds = date.getUTCSeconds();
+			seconds += date.getUTCMinutes() * 60;
+			seconds += date.getUTCHours() * 3600;
+			var utcTime = document.getElementById('utc-time');
+			utcTime.textContent = date.toUTCString();
+			var localTime = document.getElementById('local-time');
+			localTime.textContent = date.toLocaleString();
+			clockCanvasContext.beginPath();
+			clockCanvasContext.arc(clockCanvas.width / 2, clockCanvas.height / 2, MASK_RADIUS, 0, 2 * Math.PI, false);
+			clockCanvasContext.clip();
+			clockCanvasContext.translate(clockCanvas.width / 2, clockCanvas.height / 2);
+			clockCanvasContext.rotate(getRotation(seconds));
+			clockCanvasContext.drawImage(clockImage, -clockCanvas.width / 2, -clockCanvas.height / 2);
+			clockCanvasContext.restore();
+		}, INTERVAL * 1000);
+	};
 });
